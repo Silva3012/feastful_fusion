d7112d3daea7502972d608e5f5cf44af6f57fe29
Ntsika Silvano <Ntsikas@ST-MBOOK99.local>
2023-12-07 12:07:35 +0200

    Create and use riverpod provider

--- lib/screens/tab_navigation.dart
+++ lib/screens/tab_navigation.dart
@@ -1,3 +1,4 @@
+import 'package:feastful_fusion/data/dummy_data.dart';
 import 'package:feastful_fusion/models/meal.dart';
 import 'package:feastful_fusion/screens/categories_screen.dart';
 import 'package:feastful_fusion/screens/filters_screen.dart';
@@ -5,18 +6,26 @@ import 'package:feastful_fusion/screens/meals_screen.dart';
 import 'package:feastful_fusion/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
-class TabNavigation extends StatefulWidget {
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
+
+class TabNavigation extends ConsumerStatefulWidget {
   const TabNavigation({super.key});
 
   @override
-  State<TabNavigation> createState() {
+  ConsumerState<TabNavigation> createState() {
     return _TabNavigationState();
   }
 }
 
-class _TabNavigationState extends State<TabNavigation> {
+class _TabNavigationState extends ConsumerState<TabNavigation> {
   int _selectedScreenIndex = 0;
   final List<Meal> _favoriteMeals = [];
+  Map<Filter, bool> _selectedFilters = kInitialFilters;
 
   void _showSnackBarMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
@@ -49,19 +58,44 @@ class _TabNavigationState extends State<TabNavigation> {
     });
   }
 
-  void _setScreen(String indentifier) {
+  void _setScreen(String indentifier) async {
     Navigator.of(context).pop();
-    if(indentifier == "filters") {
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => const FiltersScreen())
+    if (indentifier == "filters") {
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(
+          builder: (context) => FiltersScreen(
+            currentFilters: _selectedFilters,
+          ),
+        ),
       );
+
+      setState(() {
+        _selectedFilters = result ?? kInitialFilters;
+        print(result);
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+    
     Widget activeScreen =
-        CategoriesScreen(onToggleFavorite: _toggleMealFavoritesStatus);
+        CategoriesScreen(onToggleFavorite: _toggleMealFavoritesStatus, availableMeals: availableMeals,);
     var activeScreenTitle = "Categories";
 
     if (_selectedScreenIndex == 1) {
@@ -76,7 +110,7 @@ class _TabNavigationState extends State<TabNavigation> {
       appBar: AppBar(
         title: Text(activeScreenTitle),
       ),
-      drawer:  MainDrawer(
+      drawer: MainDrawer(
         onSelectedScreen: _setScreen,
       ),
       body: activeScreen,
